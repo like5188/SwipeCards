c140c0a6724a008517d5a78311e5d18a5a4fa233
like5188 <260864866@qq.com>
2022-11-23 12:04:51 +0800

    完善

--- app/src/main/java/com/like/swipecards/app/MainActivity.kt
+++ app/src/main/java/com/like/swipecards/app/MainActivity.kt
@@ -1,11 +1,173 @@
 package com.like.swipecards.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import com.like.swipecards.SwipeCardsAdapterView
+import com.like.swipecards.app.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private val mBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        )
+    }
+    private val myAdapter: MyAdapter by lazy {
+        MyAdapter()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding
+        initView()
+        loadData()
     }
+
+    private fun initView() {
+        mBinding.swipeCardsAdapterView.isNeedSwipe = true
+        mBinding.swipeCardsAdapterView.onFlingListener = object : SwipeCardsAdapterView.OnFlingListener {
+            override fun removeFirstObjectInAdapter() {
+                this@MainActivity.myAdapter.remove(0)
+            }
+
+            override fun onExitFromLeft(dataObject: Any?) {
+                Toast.makeText(this@MainActivity, "onExitFromLeft", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onExitFromRight(dataObject: Any?) {
+                Toast.makeText(this@MainActivity, "onExitFromRight", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAdapterAboutToEmpty(itemsInAdapter: Int) {
+                if (itemsInAdapter == 0) {
+                    loadData()
+                }
+            }
+
+            override fun onScroll(progress: Float) {
+                Log.e("TAG", "progress=$progress")
+            }
+        }
+        mBinding.swipeCardsAdapterView.onItemClickListener = object : SwipeCardsAdapterView.OnItemClickListener {
+            override fun onItemClick(event: MotionEvent?, v: View?, dataObject: Any?) {
+                Toast.makeText(this@MainActivity, "onItemClick", Toast.LENGTH_SHORT).show()
+            }
+        }
+        mBinding.swipeCardsAdapterView.setAdapter(myAdapter)
+        mBinding.swipeLeft.setOnClickListener {
+            mBinding.swipeCardsAdapterView.swipeLeft()
+        }
+        mBinding.swipeRight.setOnClickListener {
+            mBinding.swipeCardsAdapterView.swipeRight()
+        }
+    }
+
+    private fun loadData() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            val list = mutableListOf<Talent>()
+            for (i in 0..5) {
+                list.add(Talent().apply {
+                    headerIcon = headerIcons[i % headerIcons.size]
+                })
+            }
+            withContext(Dispatchers.Main) {
+                myAdapter.addAll(list)
+            }
+        }
+    }
+
+    companion object {
+        private val headerIcons = intArrayOf(
+            R.drawable.i1,
+            R.drawable.i2,
+            R.drawable.i3,
+            R.drawable.i4,
+            R.drawable.i5,
+            R.drawable.i6
+        )
+    }
+
+}
+
+private class Talent {
+    var headerIcon = 0
+}
+
+private class MyAdapter : BaseAdapter() {
+    private val list = mutableListOf<Talent>()
+    fun addAll(collection: Collection<Talent>) {
+        if (isEmpty) {
+            list.addAll(collection)
+            notifyDataSetChanged()
+        } else {
+            list.addAll(collection)
+        }
+    }
+
+    fun clear() {
+        list.clear()
+        notifyDataSetChanged()
+    }
+
+    override fun isEmpty(): Boolean {
+        return list.isEmpty()
+    }
+
+    fun remove(index: Int) {
+        if (index > -1 && index < list.size) {
+            list.removeAt(index)
+            notifyDataSetChanged()
+        }
+    }
+
+    override fun getCount(): Int {
+        return list.size
+    }
+
+    override fun getItem(position: Int): Talent? {
+        return try {
+            list[position]
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var cv = convertView
+        val holder: ViewHolder
+        if (cv == null) {
+            cv = LayoutInflater.from(parent.context).inflate(R.layout.item_cardview, parent, false)
+            holder = ViewHolder()
+            cv.tag = holder
+            holder.portraitView = cv.findViewById(R.id.portrait)
+        } else {
+            holder = cv.tag as ViewHolder
+        }
+        getItem(position)?.let {
+            holder.portraitView?.setImageResource(it.headerIcon)
+        }
+        return cv!!
+    }
+
+    private class ViewHolder {
+        var portraitView: ImageView? = null
+    }
+
 }
